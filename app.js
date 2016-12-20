70757a7a56e96f0ecb10722424894248a42b8f59
Frank van Praag <frankp@FvP-LaserJet.local>
2016-12-20 22:05:43 +1100

    Added dependencies for node-persist, fs and path

--- app.js
+++ app.js
@@ -1,43 +1,91 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+var http = require('http');
+var request = require('request');
+var express = require('express');
+var app = express();
+var util = require('util');
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
+app.get('/fvp', function (req, res) {
+  res.send('Hello Frank!');
+});
+
+app.get('/create', function (req, res) {
+  console.log('Secret Santa List Creator starting...');
+  //var ids = req.param('ids').replace(/^\s+|\s+$/g,'');
+  var ids = JSON.parse(req.param.ids);
+
+  var myDraw = new SecretSanta(ids);
+  console.log(myDraw.result);
+  var myDraw = new SecretSanta(['Ryan', 'Lauren', 'Harley', 'Adrian', 'Deborah']);
+  console.log(myDraw.result);
+  // Object {Deborah: "Harley", Adrian: "Ryan", Harley: "Lauren", Lauren: "Adrian", Ryan: "Deborah"}
+
+  //var myReceiverWish = req.param('MyReceiverWish').replace(/^\s+|\s+$/g,'');
+  //var qUri = "https://au1.qualtrics.com/API/v3/directories/POOL_eYe5HRuyGLfM1WR/contacts/"+cid;
+  //var qToken = "7Ao37qRRex9xhf6xW1gEWjNTWqhg0NaSud5JDEIm";
+  //console.log('CID=',cid);
+  //console.log('myReceiverWish=',myReceiverWish);
+  //console.log('qUri=',qUri);
+  //var reqresp = request({
+      //uri: qUri,
+      //method: "PUT",
+      //headers: "{ X-API-TOKEN: qToken , Content-Type: application/json}",
+      //json: {"embeddedData": {"SSMyReceiverWish": myReceiverWish}}
+  //});
+  //console.log(reqresp);
+  console.log('Secret Santa List Creator done');
+  res.send(myDraw.result);
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+// Secret Santa Code from http://ryanknights.co.uk/javascript-secret-santa/
+function SecretSanta (participants) {
+        this.participants = participants;
+        this.copy         = this.participants.slice(),
+        this.result       = {};
+        this.equal        = true;
+        this.draw();
+}
+
+SecretSanta.prototype.listsEqual = function () {
+        for (var i = this.participants.length; i--;) {
+            if (this.participants[i] === this.copy[i]) {
+                return true;
+            }
         }
-        res.end(data);
-      }
-    });
-  }
-});
+        return false;
+}
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
-});
+SecretSanta.prototype.shuffle = function (array) {
+        var counter = array.length, temp, index;
+        while (counter > 0) {
+            index = Math.floor(Math.random() * counter);
+            counter--;
+            temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }   
+        return array;
+}   
+
+SecretSanta.prototype.draw = function () {
+        while (this.equal) {
+            this.shuffle(this.copy);
+            this.equal  = this.listsEqual();
+        }
+        for (var i = this.participants.length; i--;) {
+            this.result[this.participants[i]] = this.copy[i];
+        }
+}
+
+
