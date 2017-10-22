febaa1f2255c9ea5bf26067df27d52294913f961
FvP <github@vanpraag.com>
2017-10-22 16:16:24 +1100

    Major overhaul of start.js - not using mongodb

--- start.js
+++ start.js
@@ -6,16 +6,68 @@ const sysInfo      = require('./utils/sys-info');
 var   env          = process.env;
 var   http         = require('http');
 var   request      = require('request');
-var   express      = require('express');
-var   app          = express();
 var   util         = require('util');
+//var   util         = require('morgan');
+var express = require('express'),
+    app     = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+//Object.assign=require('object-assign');
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//app.engine('html', require('ejs').renderFile);
+//app.use(morgan('combined'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    //mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    //mongoURLLabel = "";
+
+//if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+//  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+//      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+//      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+//      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+//      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+//      mongoUser = process.env[mongoServiceName + '_USER'];
+//
+//  if (mongoHost && mongoPort && mongoDatabase) {
+//    mongoURLLabel = mongoURL = 'mongodb://';
+//    if (mongoUser && mongoPassword) {
+//      mongoURL += mongoUser + ':' + mongoPassword + '@';
+//    }
+//    // Provide UI label that excludes user id and pw
+//    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+//    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+//
+//  }
+//}
+
+//var db = null,
+//    dbDetails = new Object();
+//
+//var initDb = function(callback) {
+//  if (mongoURL == null) return;
+//
+//  var mongodb = require('mongodb');
+//  if (mongodb == null) return;
+//
+//  mongodb.connect(mongoURL, function(err, conn) {
+//    if (err) {
+//      callback(err);
+//      return;
+//    }
+//
+//    db = conn;
+//    dbDetails.databaseName = db.databaseName;
+//    dbDetails.url = mongoURLLabel;
+//    dbDetails.type = 'MongoDB';
+//
+//    console.log('Connected to MongoDB at: %s', mongoURL);
+//  });
+//};
+
+//http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
 
 app.get('/fvp', function (req, res) {
   res.send('Hello Frank.');
@@ -177,4 +229,19 @@ SecretSanta.prototype.draw = function () {
         }
 }
 
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+//initDb(function(err){
+//  console.log('Error connecting to Mongo. Message:\n'+err);
+//});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
 
