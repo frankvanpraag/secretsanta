3f37318e044ae4d1405670ac10595de25f5df00b
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2018-02-07 08:47:03 +1100

    Added /autoinc

--- start.js
+++ start.js
@@ -11,96 +11,52 @@ var   util         = require('util');
 var express = require('express'),
     app     = express();
 
-//Object.assign=require('object-assign');
-
-//app.engine('html', require('ejs').renderFile);
-//app.use(morgan('combined'))
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-    //mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    //mongoURLLabel = "";
-
-//if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-//  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-//      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-//      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-//      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-//      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-//      mongoUser = process.env[mongoServiceName + '_USER'];
-//
-//  if (mongoHost && mongoPort && mongoDatabase) {
-//    mongoURLLabel = mongoURL = 'mongodb://';
-//    if (mongoUser && mongoPassword) {
-//      mongoURL += mongoUser + ':' + mongoPassword + '@';
-//    }
-//    // Provide UI label that excludes user id and pw
-//    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-//    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-//
-//  }
-//}
-
-//var db = null,
-//    dbDetails = new Object();
-//
-//var initDb = function(callback) {
-//  if (mongoURL == null) return;
-//
-//  var mongodb = require('mongodb');
-//  if (mongodb == null) return;
-//
-//  mongodb.connect(mongoURL, function(err, conn) {
-//    if (err) {
-//      callback(err);
-//      return;
-//    }
-//
-//    db = conn;
-//    dbDetails.databaseName = db.databaseName;
-//    dbDetails.url = mongoURLLabel;
-//    dbDetails.type = 'MongoDB';
-//
-//    console.log('Connected to MongoDB at: %s', mongoURL);
-//  });
-//};
-
-//http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-//});
 
 app.get('/fvp', function (req, res) {
   res.send('Hello Frank.');
 });
 
-app.get('/draw', function (req, res) {
-  console.log('Secret Santa List Draw starting...');
-  //var ids = req.param('ids').replace(/^\s+|\s+$/g,'');
-  var ids = JSON.parse(req.param.ids);
+app.get('/autoinc', function (req, res) {
+  console.log('Autoinc...');
+  //you must first call storage.initSync    
+  storage.init( { dir:'NameValuePairs/autoinc' } ).then(function() {
+    //then start using it
+    storage.setItem('autoinc', 0)
+    .then(function() {
+      return storage.getItem('autoinc')
+    })
+    .then(function(fvalue) {
+      console.log(fvalue);
+    })
+  });
 
-  var myDraw = new SecretSanta(ids);
-  console.log(myDraw.result);
-  var myDraw = new SecretSanta(['Ryan', 'Lauren', 'Harley', 'Adrian', 'Deborah']);
-  console.log(myDraw.result);
-  // Object {Deborah: "Harley", Adrian: "Ryan", Harley: "Lauren", Lauren: "Adrian", Ryan: "Deborah"}
-
-  //var myReceiverWish = req.param('MyReceiverWish').replace(/^\s+|\s+$/g,'');
-  //var qUri = "https://au1.qualtrics.com/API/v3/directories/POOL_eYe5HRuyGLfM1WR/contacts/"+cid;
-  //var qToken = "7Ao37qRRex9xhf6xW1gEWjNTWqhg0NaSud5JDEIm";
-  //console.log('CID=',cid);
-  //console.log('myReceiverWish=',myReceiverWish);
-  //console.log('qUri=',qUri);
-  //var reqresp = request({
-      //uri: qUri,
-      //method: "PUT",
-      //headers: "{ X-API-TOKEN: qToken , Content-Type: application/json}",
-      //json: {"embeddedData": {"SSMyReceiverWish": myReceiverWish}}
-  //});
-  //console.log(reqresp);
-  console.log('Secret Santa List Draw done');
-  res.send(myDraw.result);
+  //then start using it
+  var value = storage.getItemSync('autoinc');
+  value = value + 1;
+  storage.setItem('autoinc', value)
+  console.log('Autoinc value=',value);
+  res.send({ value:value });
 });
 
+
+app.get('/get', function (req, res) {
+  console.log('Retrieving...');
+  var SurveyID = req.query.SurveyID;
+  var PanelID = req.query.PanelID;
+  var RecipientID = req.query.RecipientID;
+  var SPR = SurveyID+PanelID+RecipientID;
+  var name  = req.query.name;
+  console.log('name=',name);
+  //you must first call storage.initSync
+  storage.initSync({ dir:'NameValuePairs/'+SPR });
+  //then start using it
+  var value = storage.getItemSync(name);
+  console.log('Retrieved value=',value);
+  console.log('Retrieved.');
+  res.send({ value:value });
+});
 
 app.get('/set', function (req, res) {
   // SurveyID: SV_bODYygLhu74X2sJ
@@ -163,6 +119,35 @@ app.get('/getJSON', function (req, res) {
   console.log('Retrieved JSON: ',value);
   console.log('Retrieved.');
   res.send(value);
+});
+
+
+app.get('/draw', function (req, res) {
+  console.log('Secret Santa List Draw starting...');
+  //var ids = req.param('ids').replace(/^\s+|\s+$/g,'');
+  var ids = JSON.parse(req.param.ids);
+
+  var myDraw = new SecretSanta(ids);
+  console.log(myDraw.result);
+  var myDraw = new SecretSanta(['Ryan', 'Lauren', 'Harley', 'Adrian', 'Deborah']);
+  console.log(myDraw.result);
+  // Object {Deborah: "Harley", Adrian: "Ryan", Harley: "Lauren", Lauren: "Adrian", Ryan: "Deborah"}
+
+  //var myReceiverWish = req.param('MyReceiverWish').replace(/^\s+|\s+$/g,'');
+  //var qUri = "https://au1.qualtrics.com/API/v3/directories/POOL_eYe5HRuyGLfM1WR/contacts/"+cid;
+  //var qToken = "7Ao37qRRex9xhf6xW1gEWjNTWqhg0NaSud5JDEIm";
+  //console.log('CID=',cid);
+  //console.log('myReceiverWish=',myReceiverWish);
+  //console.log('qUri=',qUri);
+  //var reqresp = request({
+      //uri: qUri,
+      //method: "PUT",
+      //headers: "{ X-API-TOKEN: qToken , Content-Type: application/json}",
+      //json: {"embeddedData": {"SSMyReceiverWish": myReceiverWish}}
+  //});
+  //console.log(reqresp);
+  console.log('Secret Santa List Draw done');
+  res.send(myDraw.result);
 });
 
 
